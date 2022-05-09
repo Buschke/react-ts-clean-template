0548253984787336dce951b68bd33800f112621b
Buschke <github.com@buschke.com>
2022-05-09 14:47:28 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: FC<{ title: string }> = ({title}) => {
 
   return (
     <div className='App p-5 m-5 border-double hover:border-dotted'>
-      App d {title}
+      App{title}
       <FirstComponent title={title}/>  
       <button type='button' data-testid='button' onClick={() => setTestMe(testMe => 'Tested')}>{testMe}</button>
     </div>
